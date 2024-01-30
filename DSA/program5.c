b15e76301a6aa59d4efbc36ce53cc3f613715b9e
Aksh Kansagara <91257692+AkshKansagara@users.noreply.github.com>
2024-01-30 22:09:10 +0530

    Update program5.c

--- DSA/program5.c
+++ DSA/program5.c
@@ -1,52 +1,49 @@
 #include <stdio.h>
 
-#define MAX_SIZE 100  // Maximum array size
-
-void quicksort(int arr[], int low, int high) {
-    if (low < high) {
-        int pivot_index = partition(arr, low, high);
-        quicksort(arr, low, pivot_index - 1);
-        quicksort(arr, pivot_index + 1, high);
-    }
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];
-    int i = (low - 1);  // Index of smaller element
+    int i = low - 1;
 
     for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
+        if (arr[j] < pivot) {
             i++;
-            // Swap elements at indices i and j:
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            swap(arr, i, j);
         }
     }
+    swap(arr, i + 1, high);
+    return i + 1;
+}
 
-    // Swap pivot element with element at index i + 1:
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
 
-    return (i + 1);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
 
 int main() {
-    int arr[MAX_SIZE];
     int n;
-
-    printf("Enter the number of elements (max %d): ", MAX_SIZE);
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    printf("Enter %d elements:\n", n);
+    int arr[n];
+
+    printf("Enter the elements:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    quicksort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1);
 
-    printf("Sorted array:\n");
+    printf("Sorted array: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
